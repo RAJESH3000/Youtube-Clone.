3a40a0ff3d1a12e9d5656d9ed3f88a8e75d49eb1
Rushit Patel <rushitpatel163@gmail.com>
2023-05-22 07:46:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import NavComponent from "./components/Drawer";
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Main from './pages/main'
 function App() {
+  const api = "https://internship-service.onrender.com/videos?page=0";
+  const [data,setData] = useState([])
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(api);
+        setData(response.data.data.posts);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Main/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavComponent from '../components/Drawer'
+
+const Main = () => {
+  return (
+    <div>
+      <NavComponent/>
+    </div>
+  )
+}
+
+export default Main
